7cd235f8d6cb804c74dd5644e34444037fd5129c
Wellington <wellingtoncarvalho11@gmail.com>
2022-06-16 00:39:46 -0300

    feature: spring cloud gateway

---
+++ gateway/src/main/java/com/sample/gateway/config/RouteLocatorConfiguration.java
@@ -0,0 +1,20 @@
+package com.sample.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteLocatorConfiguration {
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(predicateSpec -> predicateSpec
+                        .path("/**")
+                        .uri("lb://websocket-server")
+                )
+                .build();
+    }
+}
